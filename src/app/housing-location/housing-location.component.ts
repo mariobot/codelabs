e78ae7bc35ef53c2f39dd8ff94cd924d82a73ffb
mariobot <mariobot113@hotmail.com>
2026-06-24 18:24:47 -0500

    Chapter 4 Video styles and template data.

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HouseLocation } from '../house-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section>
+      <img class="listing-photo" [src]="housingLocation.photo" alt="House" />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HouseLocation
 }
